ab6d66012b65b2d762263e33d146200eacb60cfa
Beka00best <bukhatovb@mail.ru>
2021-04-06 20:30:50 +0600

    fixed

--- 01/proxy_second.lua
+++ 01/proxy_second.lua
@@ -2,13 +2,6 @@ local fio = require('fio')
 local yaml = require('yaml')
 local uri = require('uri')
 
-local function hello() -- ответ локальному серверу
-  return {
-      status = 200,
-      body = 'hello, world'
-  }
-end
-
 local function parseConfig()
   local file, err = fio.open('config.yml', {'O_RDONLY'})
   if file == nil then
@@ -33,12 +26,20 @@ local client = require('http.client').new({max_connections = 1})
 
 local function handler()
   local requestGET = client:request('GET', 'http://'..config.proxy.bypass.host..':'..config.proxy.bypass.port,'',{timeout = 1})
-  print(requestGET.status)
-  print(requestGET.body)
+  if requestGET.body == nil then
+    requestGET.body = 'body: '.. 'nil' .. '\nreason: '..requestGET.reason .. '\nstatus: '..requestGET.status..'\n'
+  end
+  if requestGET.headers == nil then
+    requestGET.headers = 'headers: ' ..'nil\n'
+  end 
+  -- print(requestGET.status)
+  -- print(requestGET.headers)
   return {
     status = requestGET.status,
+    reason = requestGET.reason,
+    body = requestGET.body,
     headers = requestGET.headers,
-    body = requestGET.body
+    proto = requestGET.proto
   }
 end
 
@@ -50,8 +51,4 @@ local function make_response(host, port, func)
   server:start()
 end
 
-
-if config.proxy.bypass.host == 'localhost' then
-  make_response('localhost', config.proxy.bypass.port, hello) -- проверка работы прокси-сервера на локальном сервере
-end
 make_response('localhost', config.proxy.port, handler)
